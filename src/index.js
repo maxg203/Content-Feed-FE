c72aa866e85fac5dceda08305cfb9ce0e75a831e
Max Goodridge <max.goodridge@hotmail.co.uk>
2018-01-06 16:33:15 +0000

    Add placeholder image, fix tabs

--- src/index.js
+++ src/index.js
@@ -2,66 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import {
-    Row, Col, Card, CardBlock, CardTitle, CardText
+  Row, Col, Card, CardBody, CardTitle, CardText, CardImg
 } from 'reactstrap';
 import './index.css'
 
 
 class ContentFeed extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            'items': []
-        }
+    this.state = {
+      'items': []
     }
-    componentDidMount() {
-        this.getItems();
-    }
+  }
+  componentDidMount() {
+    this.getItems();
+  }
 
-    getItems() {
-        fetch('http://localhost:8000/api/item/')
-          .then(results => results.json())
-          .then(results => this.setState({'items': results}));
-    }
-    render() {
-        return (
-            <ul>
-                {this.state.items.map(function(item, index) {
-                    return (
-                        <ContentItem item={item} />
-                    )
-                }
-
-                )}
-            </ul>
-        );
-    }
+  getItems() {
+    fetch('http://localhost:8000/api/item/')
+    .then(results => results.json())
+    .then(results => this.setState({'items': results}));
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.items.map(function(item, index) {
+          return <ContentItem item={item} key={index} />
+        })}
+      </ul>
+    );
+  }
 }
 
 class ContentItem extends React.Component {
-    render() {
-        return (
-            <Row className="ContentItem">
-                <Col xs="6">
-                    <Card>
-                        <CardBlock>
-                            <CardTitle>
-                                {this.props.item.title}
-                            </CardTitle>
-                            <CardText>
-                                {this.props.item.description}
-                            </CardText>
-                        </CardBlock>
-                    </Card>
-                </Col>
-            </Row>
-        )
-    }
+  render() {
+    return (
+      <Row className="ContentItem">
+        <Col xs="3"></Col>
+        <Col xs="6">
+          <Card>
+            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"></CardImg>
+            <CardBody>
+              <CardTitle>
+                {this.props.item.title}
+              </CardTitle>
+              <CardText>
+                {this.props.item.description}
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    )
+  }
 }
 
 
 ReactDOM.render(
-    <ContentFeed />,
-    document.getElementById('root')
+  <ContentFeed />,
+  document.getElementById('root')
 );
